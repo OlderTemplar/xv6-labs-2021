f34dd0749f99162c8777a8381ea55006247bd642
OlderTemplar <381438077@qq.com>
2026-08-03 21:01:59 +0800

    implement speed up getuid()

--- kernel/proc.c
+++ kernel/proc.c
@@ -196,6 +196,23 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map the process id
+  uint64 *usyscall_pa = kalloc();
+  if (usyscall_pa == 0) {
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+  if (mappages(pagetable, USYSCALL, PGSIZE,
+    (uint64)(usyscall_pa), PTE_R | PTE_U) < 0) {
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+  *usyscall_pa = p->pid;
+
   return pagetable;
 }
 
@@ -206,6 +223,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 1);
   uvmfree(pagetable, sz);
 }
 
